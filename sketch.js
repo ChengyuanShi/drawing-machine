290a43fcdebc97982cabca652ed212d28372a9a2
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-10-14 13:38:29 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+let array = [];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220, 150, 150);
+
+  strokeWeight(5);
+  noFill();
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed){
+    // stroke(map(mouseX,0,500,0,250,true));
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+      background(0);
+    array.push([mouseX, mouseY]);
+  }
+}
+
+function keyTyped(){
+
+  if(key === 's'){
+    // save the image
+    saveCanvas('Drawing','png');
+  } else if (key === 'd'){
+    // disply image
+    background(220, 150, 150);
+    beginShape();
+  for(let i = 0; i < array.length; i++){
+  // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+  curveVertex(array[i][0], array[i][1])
+}
+endShape();
+  return false;
+}
 }
