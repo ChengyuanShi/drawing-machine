59f535694a402b1ac6dec8dc5d955316e5fa9c6f
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-10-21 16:32:09 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,30 +1,39 @@
 let array = [];
-let backgroundColor = 250;
+// let backgroundColor = 250;
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  background(backgroundColor);
+    background(150,200,250);
 
-  strokeWeight(5);
-  noFill();
+
+  // noFill();
 }
 
 function draw() {
 
-  if (mouseIsPressed){
-    // stroke(map(mouseX,0,500,0,250,true));
-      backgroundColor-= 5;
-      background(backgroundColor);
-      // line(mouseX, mouseY, pmouseX, pmouseY);
-      array.push([mouseX, mouseY]);
 
-      beginShape();
-    for(let i = 0; i < array.length; i++){
-    // line(array[i][0], array[i][1], array[i+1][0f], array[i+1][1]);
-    curveVertex(array[i][0], array[i][1])
-  }
-  endShape();
-  }
+      background(150,200,250,5);
+
+      strokeWeight(strokeWidth);
+      noiseOffset += 0.1;
+      strokeWidth = noise(noiseOffset) * 50;
+
+      stroke(map(mouseX,0,0,0,250,true));
+      // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+
+
+  //     array.push([mouseX, mouseY]);
+  //
+  //     beginShape();
+  //   for(let i = 0; i < array.length; i++){
+  //   // line(array[i][0], array[i][1], array[i+1][0f], array[i+1][1]);
+  //   curveVertex(array[i][0], array[i][1])
+  // }
+  // endShape();
+
 }
 
 function keyTyped(){
@@ -32,22 +41,24 @@ function keyTyped(){
   if(key === 's'){
     // save the image
     saveCanvas('Drawing','png');
-  } else if (key === 'd'){
-    // disply image
-    background(250);
+  } else if (key === 'c'){
+    clear();
 
-    beginShape();
-  for(let i = 0; i < array.length; i++){
-  // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
-  curveVertex(array[i][0], array[i][1])
-}
-endShape();
+    // disply image
+//     background(250);
+//
+//     beginShape();
+//   for(let i = 0; i < array.length; i++){
+//   // line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+//   curveVertex(array[i][0], array[i][1])
+// }
+// endShape();
   return false;
 }
 }
 
 function mousePressed() {
   array = [];
-  backgroundColor = 250;
+
 
 }
