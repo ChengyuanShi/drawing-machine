509beea3b28c83abbe358f21cd042aba4a565a06
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-11-03 18:23:45 -0800

    1

--- sketch.js
+++ sketch.js
@@ -2,17 +2,40 @@ let array = [];
 let backgroundColor = 250;
 let noiseOffset = 0.0;
 let strokeWidth = 5;
+let colorPicker;
+
 function setup() {
   // createCanvas(windowWidth, windowHeight);
   createCanvas(1000, 300);    // background(150,200,250);
  pixelDensity(2);
   // drawGrid();
 
-  noFill();
-  noStroke();
+
+
+  distance = 10;
+  spring = 0.5;
+  friction = 0.5;
+  size = 25;
+  diff = size/8;
+  x = y = ax = ay = a = r = f = 0;
+   noFill();
+
 }
 
 function draw() {
+
+  if (key === '1') {
+    draw1();
+  }
+  if (key === '2') {
+    draw2();
+  }
+  if (key === '3') {
+    draw3();
+  }
+}
+
+function draw1() {
 
 
       background(150,100,200,2);
@@ -42,6 +65,8 @@ if (mouseIsPressed) {
       line(mouseX , mouseY, pmouseX, pmouseY);
 
        stepSize = textWidth();
+
+
 }
 
   //     array.push([mouseX, mouseY]);
@@ -54,6 +79,82 @@ if (mouseIsPressed) {
   // endShape();
 
 }
+
+function draw2() {
+  oldR = r;
+  background(150,100,200,2);
+  if(mouseIsPressed) {
+    mX = mouseX;
+    mY = mouseY;
+    if(!f) {
+      f = 1;
+      x = mX;
+      y = mY;
+    }
+    ax += ( mX - x ) * spring;
+    ay += ( mY - y ) * spring;
+    ax *= friction;
+    ay *= friction;
+    r = 15 - sqrt( ax*ax + ay*ay ) * 0.7;
+
+    for( i = 0; i < distance; ++i ) {
+      oldX = x;
+      oldY = y;
+      x += ax / distance;
+      y += ay / distance;
+      oldR += ( r - oldR ) / distance;
+      strokeWeight( oldR );
+      stroke(map(mouseX,0,150,0,250,true));
+      line( x, y, oldX, oldY );
+    }
+  } else if(f) {
+    ax = ay = f = 0;
+  }
+}
+
+function draw3() {
+  oldR = r;
+  background(150,100,200,2);
+  if(mouseIsPressed) {
+    mX = mouseX;
+    mY = mouseY;
+    if(!f) {
+      f = 0.1;
+      x = mX;
+      y = mY;
+    }
+    ax += ( mX - x ) * spring;
+    ay += ( mY - y ) * spring;
+    ax *= friction;
+    ay *= friction;
+    a += sqrt( ax*ax + ay*ay ) - a;
+    a *= 0.6;
+    r = size - a;
+
+    for( i = 0; i < distance; ++i ) {
+      oldX = x;
+      oldY = y;
+      x += ax / distance;
+      y += ay / distance;
+      oldR += ( r - oldR ) / distance;
+      if(oldR < 1) oldR = 1;
+      strokeWeight( oldR+diff );
+
+      line( x, y, oldX, oldY );
+      strokeWeight( oldR);
+      stroke(map(mouseX,0,150,0,250,true));
+      line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
+      line( x-diff, y-diff, oldX-diff, oldY-diff );
+    }
+  } else if(f) {
+    ax = ay = f = 0;
+  }
+}
+
+
+
+
+
 
 function keyTyped(){
 
